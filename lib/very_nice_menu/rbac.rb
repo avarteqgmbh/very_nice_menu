6dc953bd8087ed58dde7c7d6d0ae61023d0b1bdd
Julian Fischer <fischer@enterprise-rails.de>
2012-01-14 17:13:19 +0100

    Implemented permission/visibility ineritance.

--- lib/very_nice_menu/rbac.rb
+++ lib/very_nice_menu/rbac.rb
@@ -6,9 +6,35 @@ module VeryNiceMenu
     @@default_permissions = [:everyone]
     
     def allowed_for?(role)
-      @options               ||= {}
-      @options[:allowed_for] ||= @@default_permissions
-      @options[:allowed_for].include?(role)
+      initialize_rbac_options
+      @options[:allowed_for].include?(role) || @options[:allowed_for].include?(:everyone)
+    end
+    
+    def allowed_for
+      initialize_rbac_options
+      @options[:allowed_for]
     end    
+    
+    protected 
+    
+    def initialize_rbac_options
+      @options                        ||= {}
+      @options[:inherit_permissions]  = true if @options[:inherit_permissions].nil?
+      set_default_permissions_unless_permissions_given    
+    end
+    
+    def set_default_permissions_unless_permissions_given
+
+      # We don't overwrite anything. It's just about setting permission in case none have been set so far.
+      return unless @options[:allowed_for].nil?
+
+      if (@options[:inherit_permissions] && @parent) then
+        # Inherit permissions
+        @options[:allowed_for]          = @parent.allowed_for 
+      else
+        # Set default permissions if unset
+        @options[:allowed_for]          = @@default_permissions
+      end
+    end
   end
 end
--- spec/very_nice_menu/rbac_spec.rb
+++ spec/very_nice_menu/rbac_spec.rb
@@ -2,9 +2,10 @@ require 'spec_helper'
 
 describe VeryNiceMenu::Rbac do
   context "Menu Permissions" do
-    it "should create a simple menu being visible to everybody" do
+    it "should create a simple menu being visible to everyone" do
       menu = VeryNiceMenu.build('Main Menu')   
       menu.allowed_for?(:everyone).should be_true
+      menu.allowed_for?(:never_thought_of_this_one_before).should be_true
     end
     
     it "should create a simple menu being visible only to admins" do
@@ -16,7 +17,7 @@ describe VeryNiceMenu::Rbac do
   end
   
   context "Menu Entry Permissions" do
-     it "should create a simple menu being visible to everybody" do
+     it "should create a simple menu being visible to everyone" do
        menu = VeryNiceMenu::Entry.new('My Entry')   
        menu.allowed_for?(:everyone).should be_true
      end
@@ -27,5 +28,56 @@ describe VeryNiceMenu::Rbac do
        menu.allowed_for?(:never_thought_of_this_one_before).should be_false
        menu.allowed_for?(:admin).should be_true
      end
-   end
+  end
+  
+  context "Visiblity Inheritance" do
+    it "should build a menu and inherit visibility" do
+      menu = VeryNiceMenu.build('Main Menu', :allowed_for => [:admin])    
+    
+      menu.submenu("Main Menu - Submenu 1") do |submenu|        
+        submenu.entry("My Entry")
+      end
+    
+      submenu = menu.submenus.first
+      entry   = submenu.entries.first
+    
+      menu.allowed_for?(:admin).should be_true
+      menu.allowed_for?(:somebody).should be_false
+      menu.allowed_for?(:everyone).should be_false
+ 
+      submenu.allowed_for?(:admin).should be_true          
+      
+      submenu.allowed_for?(:somebody).should be_false
+      menu.allowed_for?(:everyone).should be_false
+      
+      entry.allowed_for?(:admin).should be_true
+      entry.allowed_for?(:somebody).should be_false
+      menu.allowed_for?(:everyone).should be_false      
+    end
+    
+    it "should build a menu and NOT inherit visibility" do
+      menu = VeryNiceMenu.build('Main Menu', :allowed_for => [:admin], :inherit_permissions => false)    
+    
+      menu.submenu("Main Menu - Submenu 1", :inherit_permissions => false) do |submenu|        
+        submenu.entry("My Entry")
+      end
+    
+      submenu = menu.submenus.first
+      entry   = submenu.entries.first
+    
+      menu.allowed_for?(:admin).should be_true
+      menu.allowed_for?(:everyone).should be_false
+      menu.allowed_for?(:somebody).should be_false
+      
+      # Now it should not inerhit permissions and set default permissions which means visibility for everyone
+      submenu.allowed_for?(:admin).should be_true
+      
+      submenu.allowed_for?(:everyone).should be_true
+      submenu.allowed_for?(:somebody).should be_true
+      
+      entry.allowed_for?(:admin).should be_true
+      entry.allowed_for?(:everyone).should be_true
+      entry.allowed_for?(:somebody).should be_true     
+    end
+  end
 end
